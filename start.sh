c5f45ade3713a0c043ab7f1e71d1a248e4bf8073
akr14feb <kishore.a@zensar.com>
2023-09-18 10:09:38 +0530

    Added Apache Steps

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
-#To Start apache server - 14/9/2023 - changes
-# Testing CSPE Audit
-sudo systemctl start httpd
+#Install Apache http
+sudo yum install httpd
+
+
+#Start the server, and configure it to start after system reboots:
+service httpd start
+chkconfig httpd on
+
+
+#Check for configuration errors:
+service httpd configtest
+
+#Create firewall rules to allow access to the ports on which the HTTP server listens, for example:
+
+iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
+service iptables save
+
+
+#Create an index file for your webserver
+sudo bash -c 'echo This is my Web-Server running on Oracle Cloud Infrastructure >> /var/www/html/index.html'
